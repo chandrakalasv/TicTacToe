dbf6a78bc9483bdce630b9c457ccba0c54987f99
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-22 20:49:18 +0530

    added code to initialize the board

--- src/com/bridgelab/TicTocMain.java
+++ src/com/bridgelab/TicTocMain.java
@@ -3,5 +3,8 @@ package com.bridgelab;
 public class TicTocMain {
     public static void main(String[] args) {
         System.out.println("welcome to tic tac toe game");
+        TicTacToe game = new TicTacToe();
+        game.Board();
+        game.showBoard();
     }
 }
