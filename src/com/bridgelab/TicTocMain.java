d64983f2f2deffae0a82e7cd4d9844ff3dce5f52
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-22 21:00:44 +0530

    added code to show board for user

--- src/com/bridgelab/TicTocMain.java
+++ src/com/bridgelab/TicTocMain.java
@@ -5,7 +5,7 @@ public class TicTocMain {
         System.out.println("welcome to tic tac toe game");
         TicTacToe game = new TicTacToe();
         game.Board();
-        game.showBoard();
         game.selectOption();
+        game.showBoard();
     }
 }
